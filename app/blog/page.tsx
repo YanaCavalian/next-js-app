8e68d36a37ac3069d3751c2a94656e926156ed00
Yana <cavaljn@icloud.com>
2024-02-28 18:52:35 +0100

    add-search

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,36 +1,28 @@
+'use client';
+import { PostSearch } from '@/components/PostSearch';
+import { Posts } from '@/components/Posts';
+import { getAllPosts } from '@/services/getPosts';
 import { Metadata } from 'next';
-import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
-async function getData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    next: {
-      revalidate: 60,
-    },
-  });
-
-  if (!response.ok) throw new Error('Unable to fetch posts!');
-
-  const allPosts = await response.json();
-  const firstFivePosts = allPosts.slice(0, 20);
-  return firstFivePosts;
-}
-
-export const metadata: Metadata = {
+const metadata: Metadata = {
   title: 'Blog | Next App',
 };
 
 export default async function Blog() {
-  const posts = await getData();
+  const [posts, setPosts] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getAllPosts()
+      .then(setPosts)
+      .finally(() => setLoading(false));
+  }, []);
   return (
     <>
       <h1>Blog page</h1>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <PostSearch onSearch={setPosts} />
+      {loading ? <h3>Loading...</h3> : <Posts posts={posts} />}
     </>
   );
 }
